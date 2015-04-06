4a1ef8d4fbdc058f339b4000a5402d7e59dfed00
Dominik Heidler <dominik@heidler.eu>
2015-04-06 11:56:31 +0200

    update chroot from makechrootpkg
    This ensures local deps availability

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name='aurbs',
-	version='1.3.1',
+	version='1.3.2',
 	license='GPL',
 	description='Automatic AUR package building system',
 	author='Dominik Heidler',

899c5f7ea92e03738ce87d27ebae4e86d6fc2724
Dominik Heidler <dominik@heidler.eu>
2018-05-26 20:39:17 +0000

    Version 2.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name='aurbs',
-	version='2.0.0',
+	version='2.1.0',
 	license='GPL',
 	description='Automatic AUR package building system',
 	author='Dominik Heidler',
